3470117c31bd5957cb6ed26b2b40a6059e28826c
Mariano Uvalle <u.g.a.mariano@gmail.com>
2023-11-10 16:43:51 +0000

    Get rid of dead code

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,14 +1,8 @@
 package main
 
 import (
-	"encoding/base64"
-
 	"github.com/AYM1607/ccclip/internal/server/client"
 )
-
-func b64(i []byte) string {
-	return base64.StdEncoding.EncodeToString(i)
-}
 
 var apiclient *client.Client
 
